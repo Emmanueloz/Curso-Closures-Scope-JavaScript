076eabe53e8ff9b59f03e4ff2b661728da6942f8
Emmanueloz <davidozuna84@hotmail.com>
2022-07-30 18:45:05 -0500

    Debugging

---
+++ src/debug/index.js
@@ -0,0 +1,9 @@
+var a = "Hello";
+
+function hello() {
+  let b = "Hello World";
+  let c = "Hello World!";
+  debugger;//Al ejecutar este script se detendrá para debugear en el navegador * Eliminar antes de mandar a producción  
+}
+
+hello()
